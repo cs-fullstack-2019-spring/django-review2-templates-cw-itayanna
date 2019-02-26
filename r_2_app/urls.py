058bf1f7313a4e910b4f6857bef28cb648831d7e
itayanna <icage97@gmail.com>
2019-02-26 16:24:40 -0600

    classwork

---
+++ r_2_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# import endpoints_app
+from . import views
+
+urlpatterns = [
+    path('songs/', views.list_songs, name='list_songs'),
+    path('songs/<int:song_id>/', views.list_song, name='song_details'),
+    path('songs/<int:song_id>/more/', views.moreDetails, name='more_song_details')
+    ]
+
